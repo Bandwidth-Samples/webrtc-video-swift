864ce918ef8f130f6ca85e153cc2fa56bd510fc0
Mike Hamer <mhamer@bandwidth.com>
2021-01-13 13:17:41 -0500

    Initial project setup

---
+++ WebRTCVideoStoryboard/WebRTCVideoStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebRTCVideoStoryboard
+//
+//  Created by Michael Hamer on 1/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
