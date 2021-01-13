4b686f9ab22c2b73b8aaa45e5ab5d87dd97dfeee
Mike Hamer <mhamer@bandwidth.com>
2021-01-13 15:07:19 -0500

    Add get token

--- WebRTCVideoStoryboard/WebRTCVideoStoryboard/ViewController.swift
+++ WebRTCVideoStoryboard/WebRTCVideoStoryboard/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let address = "http://localhost:3000"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func getToken(completion: @escaping (String) -> Void) {
+        print("Fetching media token from server application.")
 
+        guard let url = URL(string: "\(address)/startBrowserCall") else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data,
+                  let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                  let token = json["token"] as? String else {
+                fatalError("Failed to get media token from server application.")
+            }
+            
+            DispatchQueue.main.async {
+                completion(token)
+            }
+        }.resume()
+    }
 }
 
