13de641a58b2e3a1ff79409d533a0168ccc4d4d3
Mike Hamer <mhamer@bandwidth.com>
2021-01-13 17:02:26 -0500

    First stab at including video

--- WebRTCVideoStoryboard/WebRTCVideoStoryboard/ViewController.swift
+++ WebRTCVideoStoryboard/WebRTCVideoStoryboard/ViewController.swift
@@ -6,15 +6,39 @@
 //
 
 import UIKit
+import BandwidthWebRTC
+import WebRTC
 
 class ViewController: UIViewController {
     private let address = "http://localhost:3000"
+    private let webRTC = WebRTC()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func connect(_ sender: Any) {
+        getToken { token in
+            try? self.webRTC.connect(using: token) {
+                self.webRTC.publish(audio: true, video: true) {
+                    #if arch(arm64)
+                    let localRenderer = RTCMTLVideoView(frame: .zero)
+                    let remoteRenderer = RTCMTLVideoView(frame: self.view.frame)
+                    localRenderer.videoContentMode = .scaleAspectFill
+                    remoteRenderer.videoContentMode = .scaleAspectFill
+                    #else
+                    let localRenderer = RTCEAGLVideoView(frame: .zero)
+                    let remoteRenderer = RTCEAGLVideoView(frame: self.view.frame)
+                    #endif
+                    
+                    self.webRTC.captureLocalVideo(renderer: localRenderer)
+                    self.webRTC.renderRemoteVideo(renderer: remoteRenderer)
+                }
+            }
+        }
+    }
+    
     func getToken(completion: @escaping (String) -> Void) {
         print("Fetching media token from server application.")
 
